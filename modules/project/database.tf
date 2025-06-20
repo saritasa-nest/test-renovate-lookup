28e3eee5eaa32acf12e8b6685d853779613f8e3e
Polina Malaeva <polina.malaeva@saritasa.com>
2025-06-20 11:00:13 +0700

    chore: update versions SD-1311

--- modules/project/database.tf
+++ modules/project/database.tf
@@ -1,5 +1,5 @@
 module "database" {
-  source = "git@github.com:saritasa-nest/test-renovate-lookup.git?ref=modules/database/v0.9.0"
+  source = "git@github.com:saritasa-nest/test-renovate-lookup.git?ref=modules/database/v0.0.1"
 
   var = var.project
 }
